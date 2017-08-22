0b99bc8314ecfd8e4c0f60f02e522dc14946c366
Ali Smadi <alielsmadi@gmail.com>
2017-08-22 13:09:30 +0300

    Added links2 to development tools

--- script.sh
+++ script.sh
@@ -105,6 +105,7 @@ sudo apt-get install vim -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sudo apt-get install tmux -y
 sudo apt-get install redshift -y
+sudo apt-get install links2 -y
 
 echo -e "${RED}installing gems...${NOCOLOR}"
 gem install bundler
