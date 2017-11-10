7ad0671bb41c77d0548b2be5731b6fddcdb3a30f
Ali Smadi <alielsmadi@gmail.com>
2017-11-10 23:04:07 +0200

    Set terminal emulator to reset after restarting bash

--- script.sh
+++ script.sh
@@ -106,13 +106,14 @@ sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`
 sudo apt install nodejs -y
 sudo apt install autoconf -y; sudo apt install bison -y; sudo apt install build-essential -y; sudo apt install libssl-dev -y; sudo apt install libyaml-dev -y; sudo apt install libreadline6-dev -y; sudo apt install zlib1g-dev -y; sudo apt install libncurses5-dev -y; sudo apt install libffi-dev -y; sudo apt install libgdbm3 -y; sudo apt install libgdbm-dev -y; sudo rm -rf ~/.rbenv && git clone https://github.com/rbenv/rbenv.git ~/.rbenv && echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc && echo 'eval "$(rbenv init -)"' >> ~/.bashrc && source ~/.bashrc
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-gnome-terminal -e "rbenv install 2.4.1 && rbenv global 2.4.1 && rbenv rehash && install libpq-dev -y"
+reset
+rbenv install 2.4.1 && rbenv global 2.4.1 && rbenv rehash && install libpq-dev -y
 
 echo -e "${RED}installing gems...${NOCOLOR}"
-gnome-terminal -e "gem install bundler"
-gnome-terminal -e "gem install rails"
-gnome-terminal -e "gem install rspec"
-gnome-terminal -y "gem install rubocop"
+gem install bundler
+gem install rails
+gem install rspec
+gem install rubocop
 
 echo -e "${RED}cleaning up...${NOCOLOR}"
 sudo apt -f install -y
