6699584549c0976cad0dbba64abda5b8be7d1b13
Ali Smadi <alielsmadi@gmail.com>
2017-11-17 16:15:18 +0200

    Added TODO to fix failing commands

--- script.sh
+++ script.sh
@@ -108,7 +108,7 @@ sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`
 sudo apt install nodejs -y
 sudo apt install autoconf -y; sudo apt install bison -y; sudo apt install build-essential -y; sudo apt install libssl-dev -y; sudo apt install libyaml-dev -y; sudo apt install libreadline6-dev -y; sudo apt install zlib1g-dev -y; sudo apt install libncurses5-dev -y; sudo apt install libffi-dev -y; sudo apt install libgdbm3 -y; sudo apt install libgdbm-dev -y; sudo rm -rf ~/.rbenv && git clone https://github.com/rbenv/rbenv.git ~/.rbenv && echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc && echo 'eval "$(rbenv init -)"' >> ~/.bashrc && source ~/.bashrc
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-reset
+# TODO: add a command to restart the terminal emulator here, as we already restarted bash, and the following commands will fail to execute
 rbenv install 2.4.1 && rbenv global 2.4.1 && rbenv rehash && install libpq-dev -y
 
 echo -e "${RED}installing gems...${NOCOLOR}"
