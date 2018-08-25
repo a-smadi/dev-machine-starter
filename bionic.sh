b4ba982411e06b63f3835f4fc6c99d5bb7574069
Ali Smadi <alielsmadi@gmail.com>
2018-08-25 16:44:37 +0300

    remove redshift

--- bionic.sh
+++ bionic.sh
@@ -106,7 +106,6 @@ sudo apt install vim -y && sudo apt install vim-gnome -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sudo apt install tmux -y
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-sudo apt install redshift -y
 sudo apt install links2 -y
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list' && wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add - && sudo apt update -y && sudo apt install postgresql -y && sudo apt install postgresql-contrib -y
 sudo apt install nodejs -y
